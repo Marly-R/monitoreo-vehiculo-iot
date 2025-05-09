73be514f43c5770dd5edd8feba07ef5d4e585c76
Marly-R <160815306+Marly-R@users.noreply.github.com>
2025-05-08 23:32:42 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-const API_URL = "http://44.195.81.30/api/devices";
+const API_URL = "http://3.239.186.214/api/devices";
 const tableBody = document.getElementById("deviceTableBody");
 const statusActual = document.getElementById("estadoActual");
 
